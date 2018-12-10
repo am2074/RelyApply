8fafa55d93086428383ef6ddad21281209d01a52
Arthur Malmis <almalmis1393@gmail.com>
2018-12-10 06:33:56 -0500

    added footer and fixed its position relative to the contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,7 +7,7 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(params[:contact])
 		@contact.request = request
 		if @contact.deliver			
-			flash.now[:error] = nil
+			redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
 		else
 			flash.now[:error] = 'Cannot send message.'
 			render :new
