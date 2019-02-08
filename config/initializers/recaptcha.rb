d047abc94636ba916dc27b9a5de72b4f5ede3357
Arthur Malmis <almalmis1393@gmail.com>
2019-02-08 09:39:00 -0500

    welp

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-	config.site_key  = ENV['RECAPTCHA_SITE_KEY']
-	config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
+	config.site_key  = '6Lf4t48UAAAAAJC-Np4LwYZHtZJ00mZlmlg8Aw1m'
+	config.secret_key = '6Lf4t48UAAAAAEW8q78qmLnwCHCotQl2xHJ0228-'
 end
