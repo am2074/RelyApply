32a119b7078aeae23d0bf84e310b68c144f98aa3
Arthur Malmis <almalmis1393@gmail.com>
2019-02-08 09:15:38 -0500

    trying to fix recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,6 @@
-Recaptcha.configure do |config|
-  config.site_key  = Hearback::Application.credentials.RECAPTCHA_SITE_KEY
-  config.secret_key =  Hearback::Application.credentials.RECAPTCHA_SECRET_KEY  
+if Rails.env.production?
+	Recaptcha.configure do |config|
+	  config.site_key  = Hearback::Application.credentials.RECAPTCHA_SITE_KEY
+	  config.secret_key =  Hearback::Application.credentials.RECAPTCHA_SECRET_KEY  
+	end
 end
