aa08b0827e065405f8d71cdb8df31254fdb28799
Arthur Malmis <almalmis1393@gmail.com>
2019-02-09 08:13:06 -0500

    typo

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
 	config.site_key  = Figaro.env.RECAPTCHA_SITE_KEY
-	config.secret_key = Figaro.env.ECAPTCHA_SECRET_KEY
+	config.secret_key = Figaro.env.RECAPTCHA_SECRET_KEY
 end
