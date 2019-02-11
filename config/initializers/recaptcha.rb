a97c0c194bdf72b3ec541e8c09ca339e4147d5fb
Arthur Malmis <almalmis1393@gmail.com>
2019-02-11 09:27:46 -0500

    hm 2.0

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-	config.site_key  = '6LdgTpAUAAAAAPodwrj7Mj88f_BSdChdKUi66XKN'
-	config.secret_key = '6LdgTpAUAAAAABcNnCFexm-0Kohj_H61u_R2T0fu'
+	config.site_key  = Hearback::Application.credentials.RECAPTCHA_SITE_KEY
+	config.secret_key = Hearback::Application.credentials.RECAPTCHA_SECRET_KEY
 end
