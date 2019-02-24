568fac5ec1955d05fe425a6eee6556654bdbb91d
Arthur Malmis <almalmis1393@gmail.com>
2019-02-24 18:12:08 -0500

    .

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,21 +3,31 @@ class ContactsController < ApplicationController
       @contact = Contact.new
     end
 
-    def index
-    end
     def new
       @contact = Contact.new
     end
    
     def create
-      @contact = Contact.new(params[:contact]) 
-      @contact.request = request
-        if @contact.valid? && verify_recaptcha(model: @contact)   
-          @contact.deliver_later
-          redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
-        else
-          flash.now[:error] = 'Cannot send message.'
-          render :new
-        end     
+      @contact = Contact.new(contact_params)
+      if @contact.valid?
+      MessageMailer.contact(@contact).deliver_now
+      redirect_to root_path
+      flash[:notice] = "We have received your message and will be in touch soon!"
+    else
+      flash[:notice] = "There was an error sending your message. Please try again."
+      render :new
     end
+      #@contact.request = request
+        #if @contact.valid? && verify_recaptcha(model: @contact)   
+          #@contact.deliver_later
+          #redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
+        #else
+          #flash.now[:error] = 'Cannot send message.'
+          #render :new
+        #end     
+    end
+  private
+ def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :message)
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < MailForm::Base
-	
+  include ActiveModel::Model
+  attr_accessor :first_name, :last_name, :email, :message
 	attribute  :first_name, 	 :validate => true
 	attribute  :last_name, 		 :validate => true
 	attribute  :email,	 :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
