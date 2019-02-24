49ca2d5b8de770b3182f639929f1c4dbac3fbc16
Arthur Malmis <almalmis1393@gmail.com>
2019-02-24 15:57:15 -0500

    I finally did it

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,9 +7,9 @@ class ContactsController < ApplicationController
     end
 
     def create
-      @contact = Contact.new(params[:contact])
+      @contact = Contact.new(params[:contact]) 
       @contact.request = request
-        if @contact.valid?
+        if @contact.valid? && verify_recaptcha(model: @contact)    
           redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
         else
           flash.now[:error] = 'Cannot send message.'
