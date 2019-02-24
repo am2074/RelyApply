ad51091d22743cd6a5118d9a3d6d53f80a38aef3
Arthur Malmis <almalmis1393@gmail.com>
2019-02-24 14:54:26 -0500

    blehhh

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     def create
       @contact = Contact.new(params[:contact])
       @contact.request = request
-        if @contact.delivered && verify_recaptcha(model: @contact)        
+        if @contact.delivered
           redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
         else
           flash.now[:error] = 'Cannot send message.'
