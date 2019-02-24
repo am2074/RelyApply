8a0ec748be92d13c6217aeceb2a4674f60a62d5e
Arthur Malmis <almalmis1393@gmail.com>
2019-02-24 14:58:35 -0500

    delivered doesn't exist

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     def create
       @contact = Contact.new(params[:contact])
       @contact.request = request
-        if @contact.delivered
+        if @contact.deliver
           redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
         else
           flash.now[:error] = 'Cannot send message.'
