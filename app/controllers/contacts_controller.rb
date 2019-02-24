fe801047b53e505268a2413953b9172205136878
Arthur Malmis <almalmis1393@gmail.com>
2019-02-24 15:34:17 -0500

    valid vs deliver

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     def create
       @contact = Contact.new(params[:contact])
       @contact.request = request
-        if @contact.deliver?
+        if @contact.valid?
           redirect_to root_path, notice: "Thank you for contacting us. We'll get back to you as soon as possible."
         else
           flash.now[:error] = 'Cannot send message.'
