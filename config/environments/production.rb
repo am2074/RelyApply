61908eac92ce1d06bd6d8c9fd57286a87d319d0f
Arthur Malmis <almalmis1393@gmail.com>
2019-02-25 11:35:31 -0500

    finally fixed the commit thingy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { :host => 'https://relyapply.herokuapp.com/' }
+  config.action_mailer.default_url_options = { :host => 'https://relyapply.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true  
   config.read_encrypted_secrets = true
